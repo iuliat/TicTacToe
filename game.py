4d554872969f33bb31934e59e3588692e78ed9b5
iuliat <m3_iulia_93@yahoo.com>
2014-06-08 15:07:07 +0300

    Update game.py

--- game.py
+++ game.py
@@ -1,13 +1,5 @@
 
 board = [ 1, 2, 3, 4, 5, 6, 7, 8,9]
 
-def display_board(board):
-	for i in range(0,9):
-		if board[i]%3 == 0:
-			print '%s %s' % (board[i], '|')
-			print '_ _ _ _ _ _'
-		else:
-			 print '%s %s' % (board[i], '|')
 
-display_board(board)
 
